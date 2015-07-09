9870c81d2a4c5c4ac8410036c1f155584a295180
kristianwilson <kristian.wilson@gmail.com>
2015-07-09 23:04:51 +0100

    Started to deal with status callbacks

---
+++ CallCentre/CallCentre.Core/Interfaces/IStatusCallbackUrl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallCentre.Core.Interfaces
+{
+    public interface IStatusCallback
+    {
+        string Url { get; set; }
+    }
+}
---
+++ CallCentre/CallCentre.WebAPI/Controllers/CallStatusController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Twilio.Mvc;
+
+namespace CallCentre.WebAPI.Controllers
+{
+    public class CallStatusController : ApiController
+    {
+        public void UpdateStatus(StatusCallbackRequest request)
+        {
+            // do something with this request
+        }
+    }
+}
